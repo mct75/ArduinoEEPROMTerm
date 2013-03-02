1d6fbfcbee167abfa6a0629bab55e08e16507448
Blake Dayton <mct7575@gmail.com>
2013-03-02 13:30:09 -0600

    Group read
    also, streaming

--- AET/AET.ino
+++ AET/AET.ino
@@ -1,5 +1,8 @@
+#include <Streaming.h>
 #include <EEPROM.h>
-char c;
+
+int c;
+int d;
 
 void setup(){
 Serial.begin(9600);  
@@ -8,9 +11,8 @@ Serial.begin(9600);
 void loop(){
   Serial.println("1: Read single");
   Serial.println("2: Read block");
-  Serial.println("3: Read all");
-  Serial.println("4: Write single");
-  Serial.println("5: Erase all");
+  Serial.println("3: Write single");
+  Serial.println("4: Erase all");
   
   Serial.flush(); 
   do{}while(!(Serial.available()));
@@ -20,10 +22,23 @@ void loop(){
   
   switch (c){
     case '1': Serial.println("Which address?");
-            do{}while(!(Serial.available()));
-            c=Serial.parseInt();
-            Serial.println(EEPROM.read(c));
-            break;
+              do{}while(!(Serial.available()));
+              c=Serial.parseInt();
+              Serial << "Address " << _DEC(c) << " is " << EEPROM.read(c) << endl << endl << endl;
+              break;
+            
+    case '2': Serial << "Enter start address: ";
+              do{}while(!(Serial.available()));
+              c=Serial.parseInt();
+              Serial << _DEC(c) << endl << "Enter end address: ";
+              do{}while(!(Serial.available()));
+              d=Serial.parseInt();
+              Serial << _DEC(d) << endl;
+              for(int i = c; i<=d; i++){
+                Serial << "Address " << _DEC(i) << " is " << EEPROM.read(i) << endl;
+                }
+                Serial << endl << endl;
+              break;
   }
 }
     
